bff05522b7c9e20a72023a8b582255cd6a1d596f
Zenostorm <166450511+LensonBee@users.noreply.github.com>
2026-05-27 11:56:09 +1200

    Teacher login functionality
    >No error messages yet

--- Flask/routes.py
+++ Flask/routes.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, abort, session
+from flask import Flask, render_template, abort, session, request
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer, ForeignKey, select, Table, Column, Select
+from sqlalchemy import String, Integer, ForeignKey, select, Table, Column, Select, update, values
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 DATABASE = "study.db"
 
 # in routes.py
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///c:/2026/StudyChecker/instance/study.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///study.db"
 db = SQLAlchemy(app)
 app.secret_key = "tGmesA3v77abYK3Y1UkUMlWJny6KAA"
 
@@ -20,6 +21,13 @@ class Students(Base):
     id : Mapped[int] = mapped_column(primary_key=True)
     password : Mapped[str] = mapped_column(String())
     form : Mapped[str] = mapped_column(String())
+
+
+class Admins(Base):
+    __tablename__ = "admins"
+    id : Mapped[int] = mapped_column(primary_key=True)
+    code : Mapped[str] = mapped_column(String())
+    password : Mapped[str] = mapped_column(String())
 
 
 # GENERAL ROUTES
@@ -35,9 +43,27 @@ def teacher_login():
 
     if session.get("teacher"):
         return app.redirect("/")
+    
+
 
     return render_template("teacher-login.html", title="Teacher Login")
 
+
+@app.route("/teacher-loginregister", methods=["GET", "POST"])
+def teacherloginregister():
+    code = request.form.get("username")
+    password = request.form.get("password")
+
+    user = db.session().execute(select(Admins).where(Admins.code == code)).scalar_one_or_none()
+    if not user:
+        return app.redirect("/teacher-login")
+    
+    if check_password_hash(user.password, password):
+        session["teacher"] = True
+        session.get("teacher")
+        return app.redirect("/teacher-login")
+    else:
+        return app.redirect("/teacher-login")
 
 @app.route("/student-login")
 def student_login():
